a22fa71d3c7c23121e78e501fb02510d1a7f1d20
jjustmint <123088410+jjustmint@users.noreply.github.com>
2025-09-01 21:56:10 +0700

    Add getLyrics

--- backend/src/controllers/Song.ts
+++ backend/src/controllers/Song.ts
@@ -1,3 +1,4 @@
+import { FindLyricsBySongId } from "../models/Lyrics";
 import { FindAllSong } from "../models/Song";
 import { ConstructResponse } from "../utils/responseConstructor";
 import { Context } from "hono";
@@ -13,3 +14,16 @@ export const DisplaySongsController = async (c: Context) => {
         return c.json(ConstructResponse(false, `Error: ${e}`), 500)
     }
 }
+
+export const getLyricsController = async (c: Context) => {
+    try{
+        const { songId } = await c.req.json<{ songId: number }>();
+        if(!songId){
+            return c.json(ConstructResponse(false, "Missing song id", 400));
+        }
+        const lyrics = await FindLyricsBySongId(songId);
+        return c.json(ConstructResponse(true, "Song found", lyrics ? lyrics : null), 200);
+    }catch (e) {
+        return c.json(ConstructResponse(false, `Error: ${e}`), 500)
+    }
+}   
