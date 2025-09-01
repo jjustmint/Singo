08e70cebe3206dc46d75cf3fb3326579aaca2974
Pathompong Chamchoy <119406378+APOM-Tawan@users.noreply.github.com>
2025-09-01 23:03:14 +0700

    Delete Navigation.ts

--- frontend/app/Types 2/Navigation.ts
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-    MainTabs: undefined;
-    ChooseKey: { song: { id: string; songName: string; artist: string; image: string } };
-    SettingScreen: undefined;
-    SignIn: undefined;
-  };
-  
-  export type TabKey = "home" | "stats" | "chooseKey" | "profile"  | "MusicPlayer";
