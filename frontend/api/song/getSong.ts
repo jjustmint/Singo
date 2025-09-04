931f954f9ad0aa73115401dba90ed6f8f4e2eb54
jjustmint <123088410+jjustmint@users.noreply.github.com>
2025-09-04 22:37:01 +0700

    [UPDATE] get song integratie

---
+++ frontend/api/song/getSong.ts
@@ -0,0 +1,32 @@
+import { SongType } from '../types/song';
+import { BaseResponse } from './../types/baseResponse';
+import { Axios } from "@/util/AxiosInstance";
+
+type LeaderboardResponse = BaseResponse<SongType>;
+export const getSong = async (song_id: number): Promise<LeaderboardResponse> => {
+    try {
+        const response = await Axios.post<LeaderboardResponse>(
+            "/private/getsong", {
+                song_id: song_id
+            }
+        );
+        return response.data;
+
+    } catch (e) {
+        Object.entries(e as {[key: string]: any}).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+        });
+        return {
+            success: false,
+            message: "Network error",
+            data: {
+                song_id: -1,
+                title: "",
+                key_signature: "",
+                parent_song_id: null,
+                lyrics: null,
+                singer: null,
+            },
+        }
+    }
+} 
--- frontend/api/types/song.ts
+++ frontend/api/types/song.ts
@@ -4,4 +4,5 @@ export type SongType = {
     key_signature: string,
     parent_song_id: number | null,
     lyrics: string | null,
+    singer: string | null,
 }
