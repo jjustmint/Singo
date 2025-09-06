22dbae6846f30c457e7641ea2e2dd2eb18a4a775
jjustmint <123088410+jjustmint@users.noreply.github.com>
2025-09-06 17:51:13 +0700

    [ADD] getHistory

---
+++ backend/src/types/getHistory.ts
@@ -0,0 +1,9 @@
+export type HistoryType = {
+    record_id: number,
+    user_id: number,
+    key: string,
+    user_audio_path: string,
+    accuracy_score: number,
+    created_at: Date,
+    version_id: number,
+}
